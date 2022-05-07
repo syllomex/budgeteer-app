dd7f0d0fcf078087616e290b09700a7e1d1ef4b4
Leonardo Santos <leonardojdossantos@gmail.com>
2022-05-07 15:08:46 -0300

    :sparkles: change new category button to floating button

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { ActivityIndicator, ScrollView, View } from 'react-native'
 
+import { Ionicons } from '@expo/vector-icons'
 import { colors } from '../../config/styles'
 import { useDashboard } from '../../hooks'
 import { MonthSelector } from '../../components/MonthSelector'
 import { NoContent } from '../../components/NoContent'
+import { FloatingButton } from '../../components/FloatingButton'
+import { useStore } from '../../contexts/store'
 import { Category, NewCategoryButton } from './category'
 import styles from './styles'
 
 export const Dashboard = () => {
   const { categories } = useDashboard()
 
+  const { openCategoryModal } = useStore()
+
   return (
     <View style={styles.container}>
+      <FloatingButton
+        onPress={openCategoryModal}
+        icon={props => <Ionicons {...props} name="add-outline" />}
+      />
+
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <MonthSelector />
 
@@ -30,13 +40,12 @@ export const Dashboard = () => {
               {categories.map(category => (
                 <Category key={category.uid} data={category} />
               ))}
-
-              <NewCategoryButton
-                center={!categories.length}
-                textColor={!categories.length ? 'primary' : 'muted'}
-              />
             </View>
               )}
+
+          {categories?.length === 0 && (
+            <NewCategoryButton center textColor="primary" />
+          )}
         </View>
       </ScrollView>
     </View>
