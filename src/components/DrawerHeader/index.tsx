ddf797afaafef3ab1b9ca02f79a54b2e40bbb9f2
Leonardo Santos <leonardojdossantos@gmail.com>
2022-07-02 11:20:17 -0300

    :bug: fix drawer user info

--- src/components/DrawerHeader/index.tsx
+++ src/components/DrawerHeader/index.tsx
@@ -13,14 +13,14 @@ export const DrawerHeader: React.FunctionComponent = () => {
     <View style={styles.container}>
       <View style={styles.row}>
         <Image
-          source={{ uri: user.photoURL, width: rem(6.4), height: rem(6.4) }}
+          source={{ uri: user?.photo, width: rem(6.4), height: rem(6.4) }}
           style={styles.picture}
         />
 
         <View style={styles.infoContainer}>
-          <T>{user.displayName}</T>
+          <T>{user?.name}</T>
           <T c="muted" s={1.2}>
-            {user.email}
+            {user?.email}
           </T>
         </View>
       </View>
